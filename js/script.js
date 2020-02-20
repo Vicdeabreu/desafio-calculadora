52572257808c34965f51951f3b7f2e3e86e54e54
Victor De Abreu <vmanuel.deabreu1@gmail.com>
2020-02-20 11:18:06 -0300

    Seleccionando los botones

---
+++ js/script.js
@@ -0,0 +1,93 @@
+document.getElementById("n1").addEventListener("click",n1);
+document.getElementById("n2").addEventListener("click",n2);
+document.getElementById("n3").addEventListener("click",n3);
+document.getElementById("n4").addEventListener("click",n4);
+document.getElementById("n5").addEventListener("click",n5);
+document.getElementById("n6").addEventListener("click",n6);
+document.getElementById("n7").addEventListener("click",n7);
+document.getElementById("n8").addEventListener("click",n8);
+document.getElementById("n9").addEventListener("click",n9);
+document.getElementById("n0").addEventListener("click",n0);
+document.getElementById("punto").addEventListener("click",punto);
+document.getElementById("ce").addEventListener("click",ce);
+document.getElementById("abrePar").addEventListener("click",abrePar);
+document.getElementById("cierraPar").addEventListener("click",cierraPar);
+document.getElementById("porcentaje").addEventListener("click",porcentaje);
+
+function n1(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n1').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function n2(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n2').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function n3(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n3').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function n4(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n4').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function n5(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n5').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function n6(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n6').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function n7(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n7').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function n8(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n8').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function n9(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n9').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function n0(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('n0').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function punto(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('punto').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function abre-par(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('abrePar').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function cierra-par(){
+  let actual = document.getElementById('resultado').innerHTML;
+  let sumado = document.getElementById('cierraPar').innerHTML;
+  document.getElementById('resultado').innerHTML = actual + sumado
+}
